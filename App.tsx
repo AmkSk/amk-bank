641c90c0d7ec2afb8b55bf364a9e8985ecb4362f
Andrej Martinák <andrej.martinak@vacuumlabs.com>
2023-01-27 13:30:05 +0100

    react-native-paper setup (#1)
    * Add Poppins Google Font to the project
    
    * Define basic theme color palette
    
    * Create config for fonts
    
    * Create theme config for the Paper library
    
    * Load fonts and the theme for the paper library

--- App.tsx
+++ App.tsx
@@ -1,20 +1,63 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import {
+  Poppins_400Regular,
+  Poppins_500Medium,
+  Poppins_600SemiBold,
+  Poppins_700Bold,
+  useFonts,
+} from "@expo-google-fonts/poppins";
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, View } from "react-native";
+import { Button, Provider as PaperProvider, Text } from "react-native-paper";
+
+import { theme } from "./themes/Theme";
 
 export default function App() {
+  const [fontsLoaded] = useFonts({
+    Poppins_400Regular,
+    Poppins_500Medium,
+    Poppins_600SemiBold,
+    Poppins_700Bold,
+  });
+
+  if (!fontsLoaded) {
+    // TODO show splash screen
+    return null;
+  }
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <PaperProvider theme={theme}>
+      <View style={styles.container}>
+        <Text variant="displayLarge">Display Large</Text>
+        <Text variant="displayMedium">Display Medium</Text>
+        <Text variant="displaySmall">Display small</Text>
+
+        <Text variant="headlineLarge">Headline Large</Text>
+        <Text variant="headlineMedium">Headline Medium</Text>
+        <Text variant="headlineSmall">Headline Small</Text>
+
+        <Text variant="titleLarge">Title Large</Text>
+        <Text variant="titleMedium">Title Medium</Text>
+        <Text variant="titleSmall">Title Small</Text>
+
+        <Text variant="bodyLarge">Body Large</Text>
+        <Text variant="bodyMedium">Body Medium</Text>
+        <Text variant="bodySmall">Body Small</Text>
+
+        <Text variant="labelLarge">Label Large</Text>
+        <Text variant="labelMedium">Label Medium</Text>
+        <Text variant="labelSmall">Label Small</Text>
+        <StatusBar style="auto" />
+        <Button onPress={() => {}}>Click me</Button>
+      </View>
+    </PaperProvider>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
---
+++ themes/Theme.ts
@@ -0,0 +1,15 @@
+import { configureFonts, DefaultTheme } from "react-native-paper";
+
+import { colors } from "./Colors";
+import { fontConfig } from "./Typography";
+
+export const theme = {
+  ...DefaultTheme,
+  colors: {
+    ...DefaultTheme.colors,
+    primary: colors.blue,
+    secondary: colors.yellow,
+    error: colors.red,
+  },
+  fonts: configureFonts({ config: fontConfig }),
+};
