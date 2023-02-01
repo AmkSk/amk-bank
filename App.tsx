5e62fe329dde734474d58bfaf6879856687b325a
Andrej Martinák <andrej.martinak@vacuumlabs.com>
2023-02-01 10:22:49 +0100

    Splash Screen (#4)
    * Add new splashscreen image
    
    * Add splash-screen dependency and update others
    
    * Update splash screen handling
    
    * Update Splash Screen hiding based on documentation

--- App.tsx
+++ App.tsx
@@ -14,17 +14,27 @@ import IntroScreen from './src/screens/IntroScreen'
 import LoginScreen from './src/screens/LoginScreen'
 import { theme } from './src/themes/Theme'
 import { RootStackParamList } from './src/navigation/NavigationTypes'
+import * as SplashScreen from 'expo-splash-screen'
+import { useCallback } from 'react'
+import { View } from 'react-native'
+
+SplashScreen.preventAutoHideAsync()
 
 export default function App() {
-  const [fontsLoaded] = useFonts({
+  const fontsLoaded = useFonts({
     Poppins_400Regular,
     Poppins_500Medium,
     Poppins_600SemiBold,
     Poppins_700Bold,
   })
 
+  const onLayoutRootView = useCallback(async () => {
+    if (fontsLoaded) {
+      await SplashScreen.hideAsync()
+    }
+  }, [fontsLoaded])
+
   if (!fontsLoaded) {
-    // TODO show splash screen
     return null
   }
 
@@ -33,10 +43,12 @@ export default function App() {
   return (
     <NavigationContainer>
       <PaperProvider theme={theme}>
-        <RootStack.Navigator>
-          <RootStack.Screen name={Routes.IntroScreen} component={IntroScreen} />
-          <RootStack.Screen name={Routes.LoginScreen} component={LoginScreen} />
-        </RootStack.Navigator>
+        <View style={{ flex: 1 }} onLayout={onLayoutRootView}>
+          <RootStack.Navigator>
+            <RootStack.Screen name={Routes.IntroScreen} component={IntroScreen} />
+            <RootStack.Screen name={Routes.LoginScreen} component={LoginScreen} />
+          </RootStack.Navigator>
+        </View>
       </PaperProvider>
     </NavigationContainer>
   )
