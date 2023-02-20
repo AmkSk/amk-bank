feca1e4322c68fea67b01108743df0b483de0c80
Andrej Martinák <andrej.martinak@vacuumlabs.com>
2023-02-20 13:53:00 +0100

    Use Mock API conditionally based on config

--- src/network/AmkBankClient.ts
+++ src/network/AmkBankClient.ts
@@ -1,9 +1,11 @@
 import axios from 'axios'
 import { AmkBankApiImpl } from './AmkBankApiImpl'
+import { config } from '../data/Config'
+import { AmkBankMockImpl } from './AmkBankMockImpl'
 
 const axiosClient = axios.create({
   baseURL: 'http://84.19.91.168:5000/amkbank/',
   timeout: 5000,
 })
 
-export const AmkBankApi = new AmkBankApiImpl(axiosClient)
+export const AmkBankApi = config.useMockApi ? new AmkBankMockImpl() : new AmkBankApiImpl(axiosClient)
