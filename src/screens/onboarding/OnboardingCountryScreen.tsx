1c28f61c58655a1eea2d719964bc6b522fbdd71a
Andrej Martinák <andrej.martinak@vacuumlabs.com>
2023-03-31 09:52:49 +0200

    Create custom hook for creating user account

--- src/screens/onboarding/OnboardingCountryScreen.tsx
+++ src/screens/onboarding/OnboardingCountryScreen.tsx
@@ -4,60 +4,41 @@ import { ScreenTemplate } from '../ScreenTemplate'
 import { Button, Dialog, Portal, Text } from 'react-native-paper'
 import { Strings } from '../../i18n/strings'
 import { View } from 'react-native'
-import { useContext, useEffect, useState } from 'react'
+import { useEffect, useState } from 'react'
 import { CommonStyles } from '../../themes/CommonStyles'
-import { AmkBankApi } from '../../network/AmkBankClient'
 import { Country } from '../../data/types'
 import { PaperSelect } from 'react-native-paper-select'
 import { SelectedItem } from 'react-native-paper-select/lib/typescript/interface/paperSelect.interface'
 import { useOnboardingStore } from '../../stores/onboardingStore'
 import { CommonActions } from '@react-navigation/native'
-import { LoadingContext } from '../../context/loadingContext'
-import { useError } from '../../hooks/useError'
 import { useGetCountries } from './hooks/useGetCountries'
 import { LoadingModal } from '../../components/LoadingModal'
+import { useCreateUser } from './hooks/useCreateUser'
 
 export function OnboardingCountryScreen({
   navigation,
 }: NativeStackScreenProps<RootStackParamList, Routes.OnboardingCountryScreen>) {
   const [isButtonEnabled, setIsButtonEnabled] = useState(false)
   const [isSuccessDialogShown, showSuccessDialog] = useState(false)
-  const phoneNumberPrefix = useOnboardingStore((state) => state.phonePrefix)
-  const phoneNumber = useOnboardingStore((state) => state.phoneNumber)
-  const email = useOnboardingStore((state) => state.email)
-  const dateOfBirth = useOnboardingStore((state) => state.dateOfBirth)
+
   const selectedCountry = useOnboardingStore((state) => state.countryOfResidence)
   const setSelectedCountry = useOnboardingStore((state) => state.setCountryOfResidence)
   const clearStore = useOnboardingStore((state) => state.clear)
-
-  const { showLoading } = useContext(LoadingContext)
-  const { showError } = useError()
 
   useEffect(() => {
     setIsButtonEnabled(selectedCountry !== null)
   }, [selectedCountry])
 
   const { countries, callGetCountries, isLoading: areCountriesLoading } = useGetCountries()
+  const { callCreateUser, isLoading: isCreateUserLoading } = useCreateUser()
 
   useEffect(() => {
     callGetCountries()
   }, [callGetCountries])
 
   const handleNextPress = async () => {
-    showLoading(true)
-    try {
-      await AmkBankApi.createUser(
-        phoneNumberPrefix,
-        phoneNumber,
-        email,
-        dateOfBirth?.toDateString() ?? '',
-        selectedCountry?.id ?? '',
-      )
-      showSuccessDialog(true)
-    } catch {
-      showError(Strings.onboarding_create_user_error)
-    }
-    showLoading(false)
+    const result = await callCreateUser()
+    showSuccessDialog(result)
   }
 
   const onSuccessDialogConfirmed = () => {
@@ -103,7 +84,7 @@ export function OnboardingCountryScreen({
         </Dialog>
       </Portal>
 
-      <LoadingModal visible={areCountriesLoading} />
+      <LoadingModal visible={areCountriesLoading || isCreateUserLoading} />
 
       <View style={CommonStyles.flex1} />
 
---
+++ src/screens/onboarding/hooks/useCreateUser.ts
@@ -0,0 +1,37 @@
+import { AmkBankApi } from '../../../network/AmkBankClient'
+import { Strings } from '../../../i18n/strings'
+import { useOnboardingStore } from '../../../stores/onboardingStore'
+import { useState } from 'react'
+import { useErrorStore } from '../../../stores/errorStore'
+
+export const useCreateUser = () => {
+  const [isLoading, setIsLoading] = useState(false)
+  const setError = useErrorStore((state) => state.setErrorMessage)
+
+  const phoneNumberPrefix = useOnboardingStore((state) => state.phonePrefix)
+  const phoneNumber = useOnboardingStore((state) => state.phoneNumber)
+  const email = useOnboardingStore((state) => state.email)
+  const dateOfBirth = useOnboardingStore((state) => state.dateOfBirth)
+  const selectedCountry = useOnboardingStore((state) => state.countryOfResidence)
+
+  const callCreateUser = async () => {
+    try {
+      setIsLoading(true)
+      await AmkBankApi.createUser(
+        phoneNumberPrefix,
+        phoneNumber,
+        email,
+        dateOfBirth?.toDateString() ?? '',
+        selectedCountry?.id ?? '',
+      )
+      return true
+    } catch {
+      setError(Strings.onboarding_create_user_error)
+      return false
+    } finally {
+      setIsLoading(false)
+    }
+  }
+
+  return { callCreateUser, isLoading }
+}
